3a638105e64517d2cf99682f71791a3819dc71a5
Kapil Paliwal <120932175+Kapil-2305@users.noreply.github.com>
2023-08-20 23:30:49 +0530

    Update contactFormRes.js

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -74,7 +74,7 @@ exports.contactUsEmail = (
   
   <body>
       <div class="container">
-          <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
+          <a href="https://study-notion-kapil-2305.vercel.app"><img class="logo"
                   src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
           <div class="message">Contact Form Confirmation</div>
           <div class="body">
